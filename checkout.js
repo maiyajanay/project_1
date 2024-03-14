5e2cdded34c12e7cd7a60433e1bff4b457e49471
Maiya Hoard <maiyajanay@gmail.com>
2024-03-13 20:50:55 -0400

    added tax, total, and subtotal to checkout page

--- checkout.js
+++ checkout.js
@@ -1,25 +1,29 @@
 let cart = JSON.parse(localStorage.getItem('cart'));
-
 let total = JSON.parse(localStorage.getItem("total")) || 0;
 
 function displayCart() {
-  const cartItems = document.getElementById("cart-items");
-  const subtotal = document.getElementById("subtotal");
-  const tax = document.getElementById("tax");
-  const priceTotal = document.getElementById("total");
+    const cartItems = document.getElementById("cart-items");
+    cartItems.innerHTML = "";
+    let total = 0;
+  
+    cart.forEach(item => {
+      const listItem = document.createElement("li");
+      listItem.textContent = `${item.name} - $${item.price}`
+      cartItems.appendChild(listItem);
+      total += item.price;
+    });
+  
+    const subtotal = document.getElementById("subtotal");
+    subtotal.textContent = `Subtotal: $${total}`;
+  
+    const tax = document.getElementById("tax");
+    const taxRate = 0.07;
+    const actualTax = total * taxRate;
+    tax.textContent = `Tax: $${actualTax.toFixed(2)}`;
+  
+    const priceTotal = document.getElementById("total");
+    const actualTotal = total + actualTax;
+    priceTotal.textContent = `Total: $${actualTotal.toFixed(2)}`;
+  }
 
-  cartItems.innerHTML = "";
-  cart.forEach(item => {
-    const listItem = document.createElement("li");
-    listItem.textContent = `${item.name} - $${item.price}`;
-    cartItems.appendChild(listItem);
-  });
-
-  const taxRate = 0.07;
-  const actualTax = total * taxRate;
-  subtotal.textContent = `Subtotal: $${total.toFixed(2)}`;
-  tax.textContent = `Tax: $${actualTax.toFixed(2)}`;
-  priceTotal.textContent = `Total: $${(total + actualTax).toFixed(2)}`;
-}
-
-displayCart();
+  displayCart();
